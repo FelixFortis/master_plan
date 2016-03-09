7b643d957a64749b61f49390f76ac4ab52855ed6
Adam Wenham <adamwenham64@gmail.com>
2016-03-09 07:02:52 +0000

    annotated models goal milestone user

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: milestones
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  due_date    :date
+#  description :text(65535)
+#  goal_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Milestone < ActiveRecord::Base
   belongs_to :goal
 end
